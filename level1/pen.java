fe73803915f496a255090e413fc5d9567c56a14b
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-08-14 15:24:01 +0530

    pen.java

---
+++ level1/pen.java
@@ -0,0 +1,16 @@
+public class pens {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter total number of pens: ");
+        int totalPens = sc.nextInt();
+
+        System.out.print("Enter number of students: ");
+        int students = sc.nextInt();
+
+        int pensPerStudent = totalPens / students; 
+        int remainingPens = totalPens % students;  
+
+        System.out.println("The Pen Per Student is " + pensPerStudent + "and the remaining pen not distributed is " + remainingPens);
+        sc.close();
+    }
+}
