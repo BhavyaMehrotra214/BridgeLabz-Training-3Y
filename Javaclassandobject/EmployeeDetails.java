6abd5696a00cd03cc98ae74a1ce2b4a7e374515d
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-09-02 19:45:15 +0530

    add : add program to find area and circumference

--- Javaclassandobject/EmployeeDetails.java
+++ Javaclassandobject/EmployeeDetails.java
@@ -1,5 +1,22 @@
-package Java class and object;
+package Javaclassandobject;
 
-public class EmployeeDetails {
-    
+class EmployeeDetails {
+    String name;
+    int id;
+    double salary;
+
+    // Constructor
+    EmployeeDetails(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    void displayDetails() {
+        System.out.println("Employee Details:");
+        System.out.println("Name   : " + name);
+        System.out.println("ID     : " + id);
+        System.out.println("Salary : " + salary);
+    }
 }
---
+++ Javaclassandobject/Level1/AreaofCirclemain.java
@@ -0,0 +1,8 @@
+package Javaclassandobject.Level1;
+
+public class AreaofCirclemain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(5);  
+        c1.displayDetails();
+    }
+}
