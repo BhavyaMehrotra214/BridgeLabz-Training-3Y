032875664e3c8146b0f15fa8e8725c1a2e5fbc2d
BhavyaMehrotra214 <bhavya.bm12@gmail.com>
2025-09-04 16:29:08 +0530

    add : add Circle class with constructor

---
+++ javaconstructor/CircleMain.java
@@ -0,0 +1,12 @@
+package javaconstructor;
+
+public class CircleMain {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+
+        Circle c2 = new Circle(5.0);
+
+        c1.displayDetails();
+        c2.displayDetails();
+    }
+}
